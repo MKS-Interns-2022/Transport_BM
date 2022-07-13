99391bc26410b44ade850713f7922b921339f5b2
Eyosiyas Mekbib <eyosi9000@gmail.com>
2022-07-13 13:07:19 +0300

    TB-1: Changed should keyword to is_expected

--- spec/models/transporter_spec.rb
+++ spec/models/transporter_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Transporter, type: :model do
+  subject { create(:transporter) }
 
-  it { should validate_presence_of(:code) }
-  it { should validate_uniqueness_of(:code) }
-  it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:address) }
-  it { should validate_presence_of(:contact_phone) }
+  it { is_expected.to(validate_presence_of(:code)) }
+  it { is_expected.to(validate_uniqueness_of(:code).case_insensitive) }
+  it { is_expected.to(validate_presence_of(:name)) }
+  it { is_expected.to(validate_presence_of(:address)) }
+  it { is_expected.to(validate_presence_of(:contact_phone)) }
 
 end
