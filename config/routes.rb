2ea03e881d5dcd0d0f9a767ed558763edae0b98d
nigussolomon <nigussaved@gmail.com>
2022-07-14 12:23:34 +0300

    added the route for locations - see TB-5

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # get 'route/destination:references'
   resources :routes
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :locations
   # Defines the root path route ("/")
   # root "articles#index"
 
