45984d1ca8ed5c3812bffeef32fe52ba7f357ba8
hemensol <hemensol14@yahoo.com>
2022-07-14 11:48:03 +0300

    migration file validations complete - see TB-4

--- db/migrate/20220714083425_create_routes.rb
+++ db/migrate/20220714083425_create_routes.rb
@@ -1,10 +1,10 @@
 class CreateRoutes < ActiveRecord::Migration[7.0]
   def change
     create_table :routes do |t|
-      t.string :name
-      t.references :region, null: false, foreign_key: true
-      t.references :source, null: false, foreign_key: true
-      t.references :destination, null: false, foreign_key: true
+      t.string :name, null: false
+      t.references :region, null: false, foreign_key: {to_table: :locations}
+      t.references :source, null: false, foreign_key: {to_table: :locations}
+      t.references :destination, null: false, foreign_key: {to_table: :locations}
 
       t.timestamps
     end
