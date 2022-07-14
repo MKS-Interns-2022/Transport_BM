e701585f0b21d08a4215d4c43e8c7e42e6bc2912
nigussolomon <nigussaved@gmail.com>
2022-07-14 12:24:24 +0300

    made the index test pass - see TB-5

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,8 @@
 class LocationsController < ApplicationController
+
+  def index
+    @locations = Location.all
+    data = ActiveModelSerializers::SerializableResource.new(@locations)
+    render json: { success: true, data: data}
+  end
 end
