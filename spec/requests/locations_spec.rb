6a37f748bd313a774f01d13518c9a69dc8ee85ea
nigussolomon <nigussaved@gmail.com>
2022-07-14 12:22:45 +0300

    added the index test - see TB-5

--- spec/requests/locations_spec.rb
+++ spec/requests/locations_spec.rb
@@ -1,7 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe "Locations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  let(:valid_attributes) do
+    {
+      code: Faker::Code.npi ,
+      name: Faker::Name.name ,
+      location_type: Location::REGION ,
+      description: Faker::Lorem.paragraph ,
+      ancestry: nil
+    }
   end
+
+  let(:invalid_attributes) do
+    {
+      code: Faker::Code.npi ,
+      name: Faker::Name.name ,
+      location_type: Location::REGION ,
+      description: Faker::Lorem.paragraph ,
+      ancestry: Location::ZONE
+    }
+  end
+
+  describe 'GET /index' do
+    it 'renders a succesful response' do
+      3.times { create(:location) }
+      get locations_url, as: :json
+      expect(response).to be_successful
+      result = JSON(response.body)
+      expect(result['data'].count).to eq 3
+    end
+  end
+
 end
