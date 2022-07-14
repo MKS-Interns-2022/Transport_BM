e201d6e698f5355b66e163f18f8918f25ab88cd9
nigussolomon <nigussaved@gmail.com>
2022-07-14 15:10:08 +0300

    made the show test for loactions pass - see TB-5

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,8 +1,23 @@
 class LocationsController < ApplicationController
+  before_action :set_location, only: %i[show update destroy]
 
   def index
     @locations = Location.all
     data = ActiveModelSerializers::SerializableResource.new(@locations)
     render json: { success: true, data: data}
   end
+
+  def show
+    render json: @location
+  end
+
+  private
+    def set_location
+      @location = Location.find(params[:id])
+    end
+
+    def location_params
+      params.require(:location).permit(:code, :name, :location_type, :description, :ancestry)
+    end
+
 end
