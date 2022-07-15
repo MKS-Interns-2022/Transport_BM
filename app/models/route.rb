d9460ca5531e3c7d34872791eaadd377038306c3
hemensol <hemensol14@yahoo.com>
2022-07-15 03:49:31 +0300

    model validation complete - see TB-4

--- app/models/route.rb
+++ app/models/route.rb
@@ -2,4 +2,13 @@ class Route < ApplicationRecord
   belongs_to :region, class_name: 'Location'
   belongs_to :source, class_name: 'Location'
   belongs_to :destination, class_name: 'Location'
+  
+  validates :name, presence: true
+
+  validate :validates_proper_routing
+
+  def validates_proper_routing
+    return if source != destination
+    errors.add(:base, "Source and Destination cannot be the same.") if source == destination
+  end
 end
