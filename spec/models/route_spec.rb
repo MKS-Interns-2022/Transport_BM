d2185e679bcc88b431c790a46392c3350c89ef1c
hemensol <hemensol14@yahoo.com>
2022-07-15 03:47:09 +0300

    custom test added for model - see TB-4

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Route, type: :model do
   it{is_expected.to(belong_to(:region))}
   it{is_expected.to(belong_to(:source))}
   it{is_expected.to(belong_to(:destination))}
+
+  it 'validates that source and destination are different' do
+    adama = create(:location)
+    yeka = build(:route, source: adama, destination: adama )
+    expect(yeka).not_to(be_valid)
+    expect(yeka.errors.full_messages[0]).to eq "Source and Destination cannot be the same."
+  end
 end
