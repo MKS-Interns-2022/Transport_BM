45d69bcf4413f4d4a761e5c55e98c7fa4e67ab04
nigussolomon <nigussolomon444@gmail.com>
2022-07-16 01:07:31 +0300

    made changes on the model - see TB-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_15_212927) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_15_220450) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,8 +37,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_212927) do
   end
 
   create_table "transport_plans", force: :cascade do |t|
-    t.string "reference_no"
-    t.string "plan_type"
+    t.string "reference_no", null: false
+    t.string "plan_type", null: false
     t.bigint "region_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
