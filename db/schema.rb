cb725f14c09a9ab9e58a33a0cd62b9f2e7f9c569
nigussolomon <nigussolomon444@gmail.com>
2022-07-16 12:24:05 +0300

    Generated unit_of_measure model see - TB-6

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_15_220450) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_16_092203) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_15_220450) do
     t.string "name", null: false
     t.string "address", null: false
     t.string "contact_phone", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "unit_of_measures", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "abreviation", null: false
+    t.string "unit_type", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
---
+++ spec/factories/unit_of_measures.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :unit_of_measure do
+    name { "MyString" }
+    abreviation { "MyString" }
+    unit_type { "MyString" }
+  end
+end
