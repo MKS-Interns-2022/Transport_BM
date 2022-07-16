0e0d727be688592ab64f4a532489eeb4eb78039c
nigussolomon <nigussolomon444@gmail.com>
2022-07-16 10:10:18 +0300

    Added the validations for transport plan - see TB-6

--- app/models/transport_plan.rb
+++ app/models/transport_plan.rb
@@ -5,4 +5,18 @@ class TransportPlan < ApplicationRecord
   UNPLANNED = 'Unplanned'.freeze
 
   PLAN_TYPES = [PLANNED, UNPLANNED].freeze
+
+
+  validates :reference_no, :plan_type, presence: true
+  validates :reference_no,  uniqueness: true
+
+  validates :plan_type,  inclusion: {in: PLAN_TYPES}
+  
+  validate :validates_location_type
+  def validates_location_type
+    
+    return if region.location_type == "Region"
+    errors.add(:base, "Location can only be a Region.") if region.location_type != 'Region'
+  end
+  
 end
--- spec/models/transport_plan_spec.rb
+++ spec/models/transport_plan_spec.rb
@@ -10,10 +10,10 @@ RSpec.describe TransportPlan, type: :model do
   it{is_expected.to(validate_inclusion_of(:plan_type).in_array(TransportPlan::PLAN_TYPES))}
 
   it 'validates that the plan belongs to a region' do
-    region = create(:location)
-    zone = create(:location, location_type: Location::ZONE, parent: region)
+    regiona = create(:location)
+    zone = create(:location, location_type: Location::ZONE, parent: regiona)
     plan = build(:transport_plan, region: zone)
     expect(plan).not_to(be_valid)
+    expect(plan.errors.full_messages[0]).to eq "Location can only be a Region."
   end
-
 end
