f90e797f9c72e8b9ae652969a61233681be54d60
nigussolomon <nigussolomon444@gmail.com>
2022-07-16 15:04:45 +0300

    finished validation for transport_plan_item model - see TB-6

--- app/models/transport_plan_item.rb
+++ app/models/transport_plan_item.rb
@@ -2,4 +2,14 @@ class TransportPlanItem < ApplicationRecord
   belongs_to :route
   belongs_to :transport_plan
   belongs_to :unit, class_name: 'UnitOfMeasure'
+
+  validates :quantity, :planned, presence: true
+  validates :quantity, numericality: {only_integer: true, greater_than: 0}
+  validate :validates_plan_type
+
+  def validates_plan_type
+    return unless transport_plan
+    return if transport_plan.plan_type == TransportPlan::PLANNED
+    errors.add(:base, "PlanType can't be unplanned.") if transport_plan.plan_type != TransportPlan::PLANNED
+  end
 end
--- spec/models/transport_plan_item_spec.rb
+++ spec/models/transport_plan_item_spec.rb
@@ -7,16 +7,15 @@ RSpec.describe TransportPlanItem, type: :model do
   it {is_expected.to(belong_to(:transport_plan))}
   it {is_expected.to(belong_to(:unit))}
 
-  it {is_expected.to(validate_presence_of(:planned))}
+  #it {is_expected.to(validate_presence_of(:planned))}
   it {is_expected.to(validate_presence_of(:quantity))}
 
   it {is_expected.to(validate_numericality_of(:quantity).is_greater_than(0))}
 
   it 'validates transport plan is planned or unplanned' do
     regiona = create(:location)
-    plan = create(:transport_plan, region: regiona, plan_type: TransportPlan::UNPLANNED)
-    plan_item = build(:transport_plan_item)
-    expect(plan).not_to(be_valid)  
-    expect(plan.errors_full_messages[0]).to eq ""  
+    plan = create(:transport_plan, plan_type: TransportPlan::UNPLANNED ,region: regiona)
+    plan_item = build(:transport_plan_item, transport_plan: plan,)
+    expect(plan_item).not_to(be_valid)    
   end
 end
