a6bc542d2a8fc0e0089bd8552a062dfc67e9520f
hemensol <hemensol14@yahoo.com>
2022-07-17 23:06:32 +0300

    tests and validation for models added - see TB-2

--- app/models/transport_bid.rb
+++ app/models/transport_bid.rb
@@ -1,3 +1,13 @@
 class TransportBid < ApplicationRecord
+  OPEN = 'Open'.freeze
+  CLOSED = 'Closed'.freeze
+  EVALUATED = 'Evaluated'.freeze
+  STATUSES = [OPEN, CLOSED, EVALUATED].freeze
+  
   belongs_to :transport_plan
+  validates :reference_no, :start_date, :end_date, :opening_date, :status, :bid_bond_amount, presence: true
+  validates :bid_bond_amount, numericality: { greater_than: 0 }
+  validates :status, inclusion: { in: STATUSES }
+
+
 end
