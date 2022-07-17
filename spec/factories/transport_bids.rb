7a6bc33ed4b056520a4f53327e0b6f696adbad5a
hemensol <hemensol14@yahoo.com>
2022-07-17 11:06:43 +0300

    factory setup - see TB-2

--- spec/factories/transport_bids.rb
+++ spec/factories/transport_bids.rb
@@ -1,12 +1,12 @@
 FactoryBot.define do
   factory :transport_bid do
-    reference_no { "MyString" }
-    description { "MyText" }
-    start_date { "2022-07-17" }
-    end_date { "2022-07-17" }
-    opening_date { "2022-07-17" }
-    status { "MyString" }
-    bid_bond_amount { 1 }
+    reference_no { Faker::Lorem.word }
+    description { Faker::Lorem.paragraph }
+    start_date { Faker::Date.in_date_period }
+    end_date { Faker::Date.in_date_period }
+    opening_date { Faker::Date.in_date_period }
+    status { TransportBid::OPEN }
+    bid_bond_amount { Faker::Number.positive }
     transport_plan { nil }
   end
 end
