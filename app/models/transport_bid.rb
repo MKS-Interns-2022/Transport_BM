15c1bf26064a5e9cc230c4b0575fa1dff7e295cb
hemensol <hemensol14@yahoo.com>
2022-07-18 09:19:23 +0300

    model updated - see TB-2

--- app/models/transport_bid.rb
+++ app/models/transport_bid.rb
@@ -8,6 +8,7 @@ class TransportBid < ApplicationRecord
   validates :reference_no, :start_date, :end_date, :opening_date, :status, :bid_bond_amount, presence: true
   validates :bid_bond_amount, numericality: { greater_than: 0 }
   validates :status, inclusion: { in: STATUSES }
+  validates :reference_no, uniqueness: {case_sensitive: false}
 
 
 end
