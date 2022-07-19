51acae292d7fb54f5e932009818e720e96ad3803
hemensol <hemensol14@yahoo.com>
2022-07-19 09:10:46 +0300

    bid item model validation - see TB-2

--- app/models/transport_bid_item.rb
+++ app/models/transport_bid_item.rb
@@ -2,4 +2,6 @@ class TransportBidItem < ApplicationRecord
   belongs_to :transport_bid
   belongs_to :transport_plan_item
   belongs_to :unit_of_measure
+
+  validates :quantity, presence: true, numericality: { greater_than: 0 }
 end
