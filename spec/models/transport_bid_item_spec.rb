60fe7cfa6d9212d351a84eae66098b50e3a1f6f1
hemensol <hemensol14@yahoo.com>
2022-07-19 09:08:58 +0300

    bid item model test - see TB-2

--- spec/models/transport_bid_item_spec.rb
+++ spec/models/transport_bid_item_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TransportBidItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject{ create(:transport_bid_item)}
+
+  it{is_expected.to(belong_to(:transport_bid))}
+  it{is_expected.to(belong_to(:transport_plan_item))}
+  it{is_expected.to(belong_to(:unit_of_measure))}
+  it{is_expected.to(validate_presence_of(:quantity))}
+  it{is_expected.to(validate_numericality_of(:quantity).is_greater_than(0))}
+
 end
