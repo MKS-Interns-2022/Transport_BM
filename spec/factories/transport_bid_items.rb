3527cc09f8af38cbbcca9d9d1a1f0859a16a5411
hemensol <hemensol14@yahoo.com>
2022-07-19 09:04:26 +0300

    bid item factory setup - see TB-2

--- spec/factories/transport_bid_items.rb
+++ spec/factories/transport_bid_items.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :transport_bid_item do
-    transport_bid { nil }
-    transport_plan_item { nil }
-    quantity { 1 }
-    unit_of_measure { nil }
+    transport_bid factory: :transport_bid
+    transport_plan_item factory: :transport_plan_item
+    quantity { Faker::Number.positive }
+    unit_of_measure factory: :unit_of_measure
   end
 end
