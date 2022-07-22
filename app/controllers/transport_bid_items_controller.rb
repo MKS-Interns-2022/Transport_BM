a21cac5bc0741c5bb64b7ae2b92e36ce6d3d9cce
hemensol <hemensol14@yahoo.com>
2022-07-22 12:13:11 +0300

    final commit - see TB-2

--- app/controllers/transport_bid_items_controller.rb
+++ app/controllers/transport_bid_items_controller.rb
@@ -1,13 +1,46 @@
 class TransportBidItemsController < ApplicationController
-  def transport_bid:references
+  before_action :set_transport_bid_item, only: %i[ show update ]
+
+  def index
+    @transport_bid_items = TransportBidItem.all
+    data = ActiveModelSerializers::SerializableResource.new(@transport_bid_items)
+    render json: { success: true, data: data }
   end
 
-  def transport_plan_item:references
+  def show
+    data = ActiveModelSerializers::SerializableResource.new(@transport_bid_item)
+    render json: { success: true, data: data }
   end
 
-  def quantity:integer
+  def create
+    @transport_bid_item = TransportBidItem.new(transport_bid_item_params)
+
+    if @transport_bid_item.save
+      render json: @transport_bid_item, status: :created, location: @transport_bid_item
+    else
+      render json: @transport_bid_item.errors, status: :unprocessable_entity
+    end
   end
 
-  def unit_of_measure:references
+  def update
+    if @transport_bid_item.update(transport_bid_item_params)
+      render json: @transport_bid_item
+    else
+      render json: @transport_bid_item.errors, status: :unprocessable_entity
+    end
   end
+
+  # DELETE /transport_bid_items/1
+  # def destroy
+  #   @transport_bid_item.destroy
+  # end
+
+  private
+    def set_transport_bid_item
+      @transport_bid_item = TransportBidItem.find(params[:id])
+    end
+
+    def transport_bid_item_params
+      params.require(:transport_bid_item).permit(:transport_bid_id, :transport_plan_item_id, :quantity, :unit_of_measure_id )
+    end
 end
