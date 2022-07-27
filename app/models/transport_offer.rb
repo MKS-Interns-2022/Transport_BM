053c806c685213d2a7f1d5059941a91a4d6fbb62
Eyosiyas Mekbib <eyosi9000@gmail.com>
2022-07-27 11:27:56 +0300

    Delegates added to the models - see TB-3

--- app/models/transport_offer.rb
+++ app/models/transport_offer.rb
@@ -3,4 +3,8 @@ class TransportOffer < ApplicationRecord
   belongs_to :transporter
 
   validates :offer_date, presence: true
+
+  delegate(:transport_bid_id, to: :transport_bid, prefix: true)
+  delegate(:transporter_id, to: :transporter, prefix: true)
+  delegate(:transporter_name, to: :transporter, prefix: true)
 end
--- app/models/transport_offer_item.rb
+++ app/models/transport_offer_item.rb
@@ -5,4 +5,7 @@ class TransportOfferItem < ApplicationRecord
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   validates :rank, numericality: { greater_than_or_equal_to: 0 }
 
+  delegate(:transport_bid_item_id, to: :transport_bid_item, prefix: true)
+  delegate(:transport_offer_id, to: :transport_offer, prefix: true)
+
 end
